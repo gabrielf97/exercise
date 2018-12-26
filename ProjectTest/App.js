cf8d95c6578beebd608ab9fd47446ca09e7fc77a
Gabriel <gabriiel.fmarinho@gmail.com>
2018-12-25 22:56:58 -0200

    Primeiro commit

---
+++ ProjectTest/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, TextInput, View, Button, Image} from 'react-native';
+
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View  style={styles.Body}> 
+          <Image source={require('./TronLogo.png')} style={styles.Logo} />
+            
+        <View>  
+        <TextInput style={styles.Input} placeholder="Insira seu usuário"/>
+        <TextInput style={styles.Input} placeholder="Insira sua senha"/>
+          <View style={styles.ButtonPerson} >
+            <Button title="Entrar" color="#2E8B57"/>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  Body:{
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: "#00FF7F",
+    padding: 10,
+  },
+  Input:{
+    height: 60, 
+    margin: 5, 
+    padding: 10,
+    backgroundColor: "#FFFFFF",
+  },
+  ButtonPerson:{
+    padding: 10,
+    color:"#808080",
+    
+  },
+  Logo:{
+    width: 250, 
+    height: 250,    
+    resizeMode: 'contain',   
+    alignItems: 'center'
+  },
+});
